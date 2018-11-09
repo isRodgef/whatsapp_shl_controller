21e15a8fe996baa9b1700575832ac1dffd3a1e64
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-11-09 11:16:05 +0200

    sent message from whatsapp

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import flask
 from flask import Flask, request, redirect
+from twilio.rest import Client
+from credentials import * 
 
 app = Flask(__name__)
 
@@ -7,6 +9,8 @@ app = Flask(__name__)
 
 @app.route("/whatsapp",methods=['GET', 'POST'])
 def reply():
+    client = Client(account_sid,auth_token)
+    message = client.messages.create(body= "hello",from_=sender,to=receiver)
     return ";"
 
 if __name__ == '__main__':
