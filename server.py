43b7744ad48368bbde70a0506221e146cf57e999
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-11-09 10:45:14 +0200

    added route for whatsapp gahhh gahhh

--- server.py
+++ server.py
@@ -3,5 +3,11 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
+
+
+@app.route("/whatsapp",methods=['GET', 'POST'])
+def reply():
+    return ";"
+
 if __name__ == '__main__':
 	app.run(debug=True)
