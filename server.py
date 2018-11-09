35d77d835fc054145b3bc096cf2cb80b391953ab
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-11-09 11:31:09 +0200

    added subprocess popen so it can execute commands

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import flask
 from flask import Flask, request, redirect
 from twilio.rest import Client
+from twilio import twiml
 from credentials import * 
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
@@ -9,9 +11,16 @@ app = Flask(__name__)
 
 @app.route("/whatsapp",methods=['GET', 'POST'])
 def reply():
+    
     client = Client(account_sid,auth_token)
-    message = client.messages.create(body= "hello",from_=sender,to=receiver)
+    message_body = request.form['Body']
+    process = Popen(message_body.split(),stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+
+    message = client.messages.create(body= stdout,from_=sender,to=receiver)
+   
     return ";"
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
